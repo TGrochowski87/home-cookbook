d14025a4c73278c07595219facca39607c09a4b7
TGrochowski87 <t.grochowski99@gmail.com>
2024-04-14 14:41:40 +0200

    CustomSelect WIP

--- src/pages/recipe-creation/RecipeCreationPage.tsx
+++ src/pages/recipe-creation/RecipeCreationPage.tsx
@@ -1,9 +1,11 @@
 import CustomInput from "components/CustomInput";
 import "./styles.less";
 import { useState } from "react";
+import CustomSelect from "components/CustomSelect";
 
 interface RecipeData {
   readonly name: string;
+  readonly category: string;
 }
 
 interface RecipeCreationPageProps {}
@@ -11,6 +13,7 @@ interface RecipeCreationPageProps {}
 const RecipeCreationPage = ({}: RecipeCreationPageProps) => {
   const [formData, setFormData] = useState<RecipeData>({
     name: "",
+    category: "",
   });
 
   return (
@@ -20,6 +23,7 @@ const RecipeCreationPage = ({}: RecipeCreationPageProps) => {
         value={formData.name}
         onChange={event => setFormData(prev => ({ ...prev, name: event.target.value }))}
       />
+      <CustomSelect />
     </div>
   );
 };
