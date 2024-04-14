fdc6dcdcad244ad8330fbb74760e7e776a4b58f8
TGrochowski87 <t.grochowski99@gmail.com>
2024-04-14 13:53:54 +0200

    CustomInput.tsx

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,10 @@
+import "./styles.less";
+
+interface CustomInputProps
+  extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
+
+const CustomInput = (props: CustomInputProps) => {
+  return <input className="input floating" {...props} />;
+};
+
+export default CustomInput;
--- src/pages/recipe-creation/RecipeCreationPage.tsx
+++ src/pages/recipe-creation/RecipeCreationPage.tsx
@@ -1,7 +1,27 @@
+import CustomInput from "components/CustomInput";
+import "./styles.less";
+import { useState } from "react";
+
+interface RecipeData {
+  readonly name: string;
+}
+
 interface RecipeCreationPageProps {}
 
 const RecipeCreationPage = ({}: RecipeCreationPageProps) => {
-  return <div>XD</div>;
+  const [formData, setFormData] = useState<RecipeData>({
+    name: "",
+  });
+
+  return (
+    <div className="page-layout-column recipe-creation-page">
+      <h1>Nowy przepis</h1>
+      <CustomInput
+        value={formData.name}
+        onChange={event => setFormData(prev => ({ ...prev, name: event.target.value }))}
+      />
+    </div>
+  );
 };
 
 export default RecipeCreationPage;
