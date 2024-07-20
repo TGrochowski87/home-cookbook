97b3b8e5a0d2526d4e540ed98e0492b3cd82e525
TGrochowski87 <t.grochowski99@gmail.com>
2024-07-20 16:42:21 +0200

    ok, working with self-signed certs is still a pain

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import * as GET from "./GET/requests";
 import * as POST from "./POST/requests";
 
 // Type in "thisisunsafe"!
-export const baseUrl = "https://192.168.0.164:3000";
+export const baseUrl = "http://192.168.0.164:3000";
 
 const api = { get: { ...GET }, post: { ...POST } };
 
