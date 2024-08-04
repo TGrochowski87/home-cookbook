b0a027b224f01a4a399de5625dc54a8b2aecccbc
TGrochowski87 <t.grochowski99@gmail.com>
2024-08-04 12:45:16 +0200

    data script

---
+++ database/scripts/2_Data.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (id, name, color) 
+VALUES 
+    (1, 'obiad', '#9821C5'),
+    (2, 'deser', '#3BE198'),
+    (3, 'śniadanie/kolacja', '#D9D12B'),
+    (4, 'zupa', '#D62929'),
+    (5, 'sos', '#29C9D6'),
+    (6, 'alkohol', '#2943D6'),
+    (7, 'inne', '#E13BCB');
