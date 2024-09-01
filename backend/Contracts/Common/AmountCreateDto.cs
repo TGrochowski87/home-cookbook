ea48b0ddc27772c17a9096bc37831b802203a0f1
TGrochowski87 <t.grochowski99@gmail.com>
2024-09-01 20:23:20 +0200

    missed one DTO

---
+++ backend/Contracts/Common/AmountCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.Contracts.Common;
+
+public class AmountCreateDto
+{
+  [FromForm(Name = "value")] 
+  public required string Value { get; set; }
+
+  [FromForm(Name = "unit")] 
+  public string? Unit { get; set; }
+}
