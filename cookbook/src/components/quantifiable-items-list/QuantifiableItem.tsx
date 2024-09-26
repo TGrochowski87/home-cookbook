8eb45f92306236917dea86d4a826e4750feefb59
TGrochowski87 <t.grochowski99@gmail.com>
2024-09-26 16:42:25 +0200

    QuantifiableItemsList based on HighlightingList

--- cookbook/src/components/quantifiable-items-list/QuantifiableItem.tsx
+++ cookbook/src/components/quantifiable-items-list/QuantifiableItem.tsx
@@ -2,25 +2,14 @@ import { Amount } from "api/GET/DTOs";
 import QuantifiableItemData from "../../models/QuantifiableItemData";
 import ActionData from "../../models/ActionData";
 import Action from "./Action";
-import { useEffect, useRef } from "react";
 
 interface QuantifiableItemProps {
   readonly data: QuantifiableItemData;
-  readonly isHighlighted: boolean;
-  readonly handleTouchStart: (item: QuantifiableItemData["key"]) => void;
   readonly rightSideAction?: ActionData;
   readonly leftSideAction?: ActionData;
 }
 
-const QuantifiableItem = ({
-  data,
-  isHighlighted,
-  handleTouchStart,
-  leftSideAction,
-  rightSideAction,
-}: QuantifiableItemProps) => {
-  const listItemRef = useRef<HTMLLIElement>(null);
-
+const QuantifiableItem = ({ data, leftSideAction, rightSideAction }: QuantifiableItemProps) => {
   const displayAmount = (amount: Amount): JSX.Element => {
     if (amount.unit) {
       return (
@@ -34,28 +23,15 @@ const QuantifiableItem = ({
     }
   };
 
-  useEffect(() => {
-    const localTouchStartHandler = () => handleTouchStart(data.key);
-
-    listItemRef.current?.addEventListener("touchstart", localTouchStartHandler, { passive: false });
-
-    return () => {
-      listItemRef.current?.removeEventListener("touchstart", localTouchStartHandler);
-    };
-  }, [listItemRef.current, handleTouchStart]);
-
   return (
-    <li
-      ref={listItemRef}
-      data-key={data.key}
-      className={`quantifiable-list-item ${isHighlighted ? "highlighted" : ""}`}>
+    <>
       {leftSideAction && <Action item={data} action={leftSideAction} isLeftSide />}
       <div className={`quantifiable-data ${data.checked ? "checked" : ""}`}>
         <p>{data.name}</p>
         {displayAmount(data.amount)}
         {rightSideAction && <Action item={data} action={rightSideAction} />}
       </div>
-    </li>
+    </>
   );
 };
 
