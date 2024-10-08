6d3b367ba84fdb1e6fc1a251bf274b17c3288e0d
TGrochowski87 <t.grochowski99@gmail.com>
2024-10-08 21:51:21 +0200

    frontend changes

--- cookbook/src/api/PUT/requests.ts
+++ cookbook/src/api/PUT/requests.ts
@@ -2,6 +2,8 @@ import { RecipeCreateDto } from "api/POST/DTOs";
 import { prepareRecipeFormData } from "api/POST/requests";
 import { baseUrl } from "api/api";
 import axios from "axios";
+import { ShoppingListUpdateDto } from "./DTOs";
+import { ShoppingListDetailsGetDto } from "api/GET/DTOs";
 
 export const updateRecipe = async (recipeId: number, data: RecipeCreateDto): Promise<void> => {
   const formData = prepareRecipeFormData(data);
@@ -9,5 +11,14 @@ export const updateRecipe = async (recipeId: number, data: RecipeCreateDto): Pro
   const response = await axios.put(url, formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
+  return response.data;
+};
+
+export const updateShoppingList = async (
+  shoppingListId: number,
+  dto: ShoppingListUpdateDto
+): Promise<ShoppingListDetailsGetDto> => {
+  const url = `${baseUrl}/shopping-lists/${shoppingListId}`;
+  const response = await axios.put(url, dto);
   return response.data;
 };
--- cookbook/src/pages/shopping-list/ShoppingListPage.tsx
+++ cookbook/src/pages/shopping-list/ShoppingListPage.tsx
@@ -4,12 +4,12 @@ import TitledSection from "components/TitledSection";
 import { useLoaderData } from "react-router-dom";
 import SublistTitle from "./SublistTitle";
 import QuantifiableItemsList from "components/quantifiable-items-list/QuantifiableItemsList";
-import QuantifiableItemData from "models/QuantifiableItemData";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import EditableQuantifiableItemsList from "components/quantifiable-items-list/EditableQuantifiableItemsList";
 import { ShoppingList, ShoppingListSublist } from "models/ShoppingList";
 import mapper from "mapper";
 import InfoModal from "./InfoModal";
+import useShoppingListUpdateManagement from "./useShoppingListUpdateManagement";
 import "./styles.less";
 
 export async function loader({ params }: any) {
@@ -19,75 +19,82 @@ export async function loader({ params }: any) {
 
 const ShoppingListPage = () => {
   const shoppingListFromLoader = useLoaderData() as ShoppingListDetailsGetDto;
-  const [shoppingList, setShoppingList] = useState<ShoppingList>(prepareShoppingList(shoppingListFromLoader));
+  const [shoppingList, setShoppingList] = useState<ShoppingList>(mapper.map.toShoppingList(shoppingListFromLoader));
 
-  const checkboxClickHandler = (sublistId: number): ((item: QuantifiableItemData) => void) => {
-    return (item: QuantifiableItemData) => {
-      setShoppingList(prev => ({
-        ...prev,
-        sublists: prev.sublists!.map(sub =>
-          sub.id === sublistId
-            ? { ...sub, items: sub.items.map(i => (i.key === item.key ? { ...i, checked: !i.checked } : i)) }
-            : sub
-        ),
-      }));
-    };
-  };
+  const updateShoppingList: React.Dispatch<React.SetStateAction<ShoppingList>> = (
+    newState: ShoppingList | ((prevState: ShoppingList) => ShoppingList)
+  ) => {
+    setShoppingList(newState);
 
-  // Currently there can only be one manual sublist
-  const removeItemHandler = (item: QuantifiableItemData): void => {
-    setShoppingList(prev => ({
-      ...prev,
-      sublists: prev.sublists!.map(sub =>
-        sub.recipeId === null ? { ...sub, items: sub.items.filter(i => i.key !== item.key) } : sub
-      ),
-    }));
-  };
-
-  const onIncrementHandler = async (sublist: ShoppingListSublist) => {
-    await api.patch.updateShoppingSublistCount(sublist.id, sublist.count + 0.5);
-    const updatedShoppingList = await api.get.getShoppingList(shoppingList.id);
-    setShoppingList(prepareShoppingList(updatedShoppingList));
-  };
-
-  const onDecrementHandler = async (sublist: ShoppingListSublist) => {
-    if (sublist.count === 0.5) {
-      await api.delete.deleteShoppingListSublist(sublist.id);
-    } else {
-      await api.patch.updateShoppingSublistCount(sublist.id, sublist.count - 0.5);
+    if (apiCallTimeoutId.current === undefined) {
+      apiCallTimeoutId.current = setTimeout(async () => await saveBeforeUnmount(), 5000);
     }
-
-    const updatedShoppingList = await api.get.getShoppingList(shoppingList.id);
-    setShoppingList(prepareShoppingList(updatedShoppingList));
   };
 
-  const setManualSublistItems = (items: readonly QuantifiableItemData[]): void => {
-    setShoppingList(prev => ({
-      ...prev,
-      sublists: prev.sublists!.map(sub => (sub.recipeId === null ? { ...sub, items } : sub)),
-    }));
+  const handle = useShoppingListUpdateManagement(updateShoppingList);
+
+  // Fields related to delayed saving.
+  const apiCallTimeoutId = useRef<number>();
+
+  const saveBeforeUnmount = async () => {
+    // No error handling because no errors are expected.
+    const updatedShoppingList = await api.put.updateShoppingList(
+      shoppingList.id,
+      mapper.map.toShoppingListUpdateDto(shoppingList)
+    );
+    setShoppingList(mapper.map.toShoppingList(updatedShoppingList));
+    apiCallTimeoutId.current = undefined;
   };
 
-  const manualSublist: ShoppingListSublist = shoppingList.sublists!.find(s => s.recipeId === null)!;
+  // Call API immediately on unmount.
+  useEffect(() => {
+    return () => {
+      saveBeforeUnmount();
+    };
+  }, []);
+
+  // Send quick API call before page/browser closing.
+  useEffect(() => {
+    const saveBeforeUnloadWithBeacon = () => {
+      if (apiCallTimeoutId.current === undefined) {
+        return;
+      }
+
+      const dto = mapper.map.toShoppingListUpdateDto(shoppingList);
+      navigator.sendBeacon(`/api/shopping-lists/${shoppingList.id}`, JSON.stringify(dto));
+    };
+
+    window.addEventListener("beforeunload", saveBeforeUnloadWithBeacon);
+
+    return () => {
+      window.removeEventListener("beforeunload", saveBeforeUnloadWithBeacon);
+    };
+  }, [shoppingList]);
+
+  const manualSublist: ShoppingListSublist = shoppingList.sublists.find(s => s.recipeId === null)!;
 
   return (
     <div className="page shopping-list-page">
       <span className="header">
         <h1>{shoppingList.name}</h1>
-        <InfoModal shoppingListInfo={shoppingList} renameHandler={name => setShoppingList({ ...shoppingList, name })} />
+        <InfoModal shoppingListInfo={shoppingList} renameHandler={handle.nameUpdate} />
       </span>
 
-      {shoppingList
-        .sublists!.filter(sublist => sublist.recipeId)
+      {shoppingList.sublists
+        .filter(sublist => sublist.recipeId)
         .map(sublist => (
           <TitledSection
             key={sublist.id}
             title={
-              <SublistTitle sublist={sublist} onIncrement={onIncrementHandler} onDecrement={onDecrementHandler} />
+              <SublistTitle
+                sublist={sublist}
+                onIncrement={handle.sublistIncrementCount}
+                onDecrement={handle.sublistDecrementCount}
+              />
             }>
             <QuantifiableItemsList
               items={sublist.items}
-              rightSideAction={{ type: "check", callback: checkboxClickHandler(sublist.id) }}
+              rightSideAction={{ type: "check", callback: handle.checkboxClick(sublist.id) }}
             />
           </TitledSection>
         ))}
@@ -95,22 +102,13 @@ const ShoppingListPage = () => {
       <TitledSection title={"Manualnie dodane"}>
         <EditableQuantifiableItemsList
           items={manualSublist.items}
-          setItems={items => setManualSublistItems(items)}
-          leftSideAction={{ type: "remove", callback: removeItemHandler }}
-          rightSideAction={{ type: "check", callback: checkboxClickHandler(manualSublist.id) }}
+          setItems={items => handle.manualSublistItemsUpdate(items)}
+          leftSideAction={{ type: "remove", callback: handle.listItemDelete }}
+          rightSideAction={{ type: "check", callback: handle.checkboxClick(manualSublist.id) }}
         />
       </TitledSection>
     </div>
   );
-};
-
-const prepareShoppingList = (shoppingListDto: ShoppingListDetailsGetDto): ShoppingList => {
-  const shoppingList: ShoppingList = mapper.map.toShoppingList(shoppingListDto);
-  const sortedShoppingList: ShoppingList = {
-    ...shoppingList,
-    sublists: shoppingList.sublists!.toSorted((a, b) => a.id - b.id),
-  };
-  return sortedShoppingList;
 };
 
 export default ShoppingListPage;
