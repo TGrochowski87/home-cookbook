9986be7c488f6c90a8a7fdc4102b1a31806d80bc
TGrochowski87 <t.grochowski99@gmail.com>
2024-10-13 12:02:13 +0200

    pad start with 0 in shopping list dates

--- cookbook/src/utilities/formatDate.ts
+++ cookbook/src/utilities/formatDate.ts
@@ -1,10 +1,13 @@
 const formatDate = (date: Date | string): string => {
   const actualDate = typeof date === "string" ? new Date(date) : date;
 
-  return `${actualDate.getHours().toString().padStart(2, "0")}:${actualDate
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")} ${actualDate.getDate()}.${actualDate.getMonth() + 1}.${actualDate.getFullYear()}`;
+  return `${padLeft(actualDate.getHours())}:${actualDate.getMinutes().toString().padStart(2, "0")} ${padLeft(
+    actualDate.getDate()
+  )}.${padLeft(actualDate.getMonth() + 1)}.${actualDate.getFullYear()}`;
+};
+
+const padLeft = (value: number): string => {
+  return value.toString().padStart(2, "0");
 };
 
 export default formatDate;
