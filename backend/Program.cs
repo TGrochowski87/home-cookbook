f4f22793fdfb70cc0729f4540d06eb7d43329c43
TGrochowski87 <t.grochowski99@gmail.com>
2024-11-14 20:03:06 +0100

    HTTP logging and ProblemDetails

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics;
 using Cookbook.DataAccess;
 using Cookbook.Extensions;
 using FluentValidation;
+using Microsoft.AspNetCore.HttpLogging;
 using SharpGrip.FluentValidation.AutoValidation.Endpoints.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,12 +11,29 @@ builder.Logging.AddSimpleConsole(options =>
 {
   options.TimestampFormat = "[HH:mm:ss] ";
 });
+builder.Services.AddHttpLogging(logging =>
+{
+  logging.LoggingFields = HttpLoggingFields.RequestPropertiesAndHeaders | HttpLoggingFields.RequestQuery |
+                          HttpLoggingFields.RequestBody | HttpLoggingFields.ResponsePropertiesAndHeaders |
+                          HttpLoggingFields.ResponseBody | HttpLoggingFields.Duration;
+  logging.MediaTypeOptions.AddText("application/json");
+  logging.RequestBodyLogLimit = 4096;
+  logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Services.AddProblemDetails(options =>
+{
+  options.CustomizeProblemDetails = (context) =>
+  {
+    if (context.ProblemDetails.Extensions.ContainsKey("traceId") == false)
+    {
+      context.ProblemDetails.Extensions.Add("traceId", Activity.Current?.Id ?? context.HttpContext.TraceIdentifier);
+    }
+  };
+});
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-  options.SupportNonNullableReferenceTypes();
-});
+builder.Services.AddSwaggerGen(options => { options.SupportNonNullableReferenceTypes(); });
 
 builder.Services.AddCors(options =>
 {
@@ -36,6 +55,10 @@ builder.Services
   .AddFluentValidationAutoValidation();
 
 var app = builder.Build();
+
+app.UseHttpLogging();
+
+app.UseExceptionHandler();
 
 if (app.Environment.IsDevelopment())
 {
