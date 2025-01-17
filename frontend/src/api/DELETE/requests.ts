1eb60d480d6daf3d6455ac7c2ce071ebe3fe2588
TGrochowski87 <t.grochowski99@gmail.com>
2025-01-17 23:02:45 +0100

    removed call for deleted endpoint

--- frontend/src/api/DELETE/requests.ts
+++ frontend/src/api/DELETE/requests.ts
@@ -1,12 +1,6 @@
 import { BaseUrl } from "api/api";
 import axios from "axios";
 
-export const deleteShoppingListSublist = async (sublistId: number): Promise<void> => {
-  const url = `${BaseUrl}/shopping-lists/sublists/${sublistId}`;
-  const response = await axios.delete(url);
-  return response.data;
-};
-
 export const deleteShoppingList = async (listId: number, resourceStateTimestamp: string): Promise<void> => {
   const url = `${BaseUrl}/shopping-lists/${listId}`;
   const response = await axios.delete(url, { headers: { "If-Unmodified-Since": resourceStateTimestamp } });
