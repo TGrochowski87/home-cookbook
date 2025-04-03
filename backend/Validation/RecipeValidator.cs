ca2b76efe027d95164c18b94ef3ffe97d24b90f1
TGrochowski87 <t.grochowski99@gmail.com>
2025-04-03 19:54:14 +0200

    6MB on backend side

--- backend/Validation/RecipeValidator.cs
+++ backend/Validation/RecipeValidator.cs
@@ -5,7 +5,7 @@ namespace Cookbook.Validation;
 
 public class RecipeValidator : AbstractValidator<RecipeCreateDto>
 {
-  private const int _maxFileSize = 3145728;
+  private const int _maxFileSize = 6291456;
   private readonly List<string> _allowedImageFormats = ["image/jpeg", "image/png", "image/webp"];
 
   public RecipeValidator()
@@ -20,7 +20,7 @@ public class RecipeValidator : AbstractValidator<RecipeCreateDto>
     {
       RuleFor(recipe => recipe.Image!)
       .Must(image => image.Length <= _maxFileSize)
-      .WithMessage(recipe => $"The image size limit is 3MB. Size of the provided image: {recipe.Image!.Length / 1024}KB.")
+      .WithMessage(recipe => $"The image size limit is 6MB. Size of the provided image: {recipe.Image!.Length / 1024}KB.")
       .Must(image => _allowedImageFormats.Contains(image.ContentType))
       .WithMessage(recipe => $"The image must be in one of the following formats: {string.Join(", ", _allowedImageFormats)}. " +
                              $"Format of the provided image: {recipe.Image!.ContentType}.");
